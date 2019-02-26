fd9049981fce3678e60803571e52328246f1e39e
Tom Harris <tharris@harrisnj.net>
2019-02-26 12:51:39 -0500

    Add create_from_dict method

--- pyinsteon/messages/user_data.py
+++ pyinsteon/messages/user_data.py
@@ -4,6 +4,12 @@ import binascii
 
 _LOGGER = logging.getLogger(__name__)
 
+def create_from_dict(user_data_dict: dict):
+    """Create a UserData object from a dictionary."""
+    empty = create_empty()
+    ud = _dict_to_dict(empty, user_data_dict)
+    return ud
+
 
 def create_empty(val=0x00):
     """Create an empty Userdata object.
@@ -93,8 +99,8 @@ class UserData():
         """Test if the current user data is not equal to another instance."""
         return bool(self != other)
 
-    def __str__(self):
-        """Emit the address in human-readible format (AA.BB.CC)."""
+    def __repr__(self):
+        """Emit the user data in debug format (AA.BB.CC)."""
         hex_str = binascii.hexlify(bytes(self)).decode()
         strout = ''
         first = True
@@ -105,6 +111,16 @@ class UserData():
                 strout = strout + '.'
             strout = strout + hex_str[i:i + 2]
         return strout
+
+    def __str__(self):
+        """Emit the user data in human readable format."""
+        from ..utils import vars_to_string
+        data = []
+        for i in range(1, 15):
+            key = 'd{}'.format(i)
+            val = self._user_data[key]
+            data.append((key, val))
+        return vars_to_string(data)
 
     def __bytes__(self):
         """Emit the address in bytes format."""
@@ -124,3 +140,33 @@ class UserData():
     def to_dict(self):
         """Return user_data as a dict object."""
         return self._user_data
+
+
+    def set_checksum(self, cmd1: int, cmd2: int, version=2):
+        """Set the checksum."""
+        data_sum = cmd1 + cmd2
+        for i in range(1, 14):
+            data_sum += self._user_data['d{:d}'.format(i)]
+        chksum = 0xff - (data_sum & 0xff) + 1
+        self._user_data['d14'] = chksum
+
+
+    def set_crc(self, cmd1: int, cmd2: int):
+        """Set Userdata[13] and Userdata[14] to the CRC value."""
+        data1 = bytes(cmd1) 
+        data2 = bytes(cmd2)
+        data3 = bytes(self)[0:13]
+        data = b''.join([data1, data2, data3])
+        crc = int(0)
+        for b in data:
+            # pylint: disable=unused-variable
+            for bit in range(0, 8):
+                fb = b & 0x01
+                fb = fb ^ 0x01 if (crc & 0x8000) else fb
+                fb = fb ^ 0x01 if (crc & 0x4000) else fb
+                fb = fb ^ 0x01 if (crc & 0x1000) else fb
+                fb = fb ^ 0x01 if (crc & 0x0008) else fb
+                crc = ((crc << 1) | fb) & 0xffff
+                b = b >> 1
+        self._user_data['d13'] = (crc >> 8) & 0xff
+        self._user_data['d14'] = crc & 0xff
