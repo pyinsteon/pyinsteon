950c1e2ecc403c3649e810d59be4a3e88c05243b
Tom Harris <tharris@harrisnj.net>
2019-03-06 22:04:36 -0500

    Create modem device

--- pyinsteon/devices/modem.py
+++ pyinsteon/devices/modem.py
@@ -1,9 +1,8 @@
 """Insteon Modem Base Class."""
 from abc import ABCMeta, abstractmethod
-
+import asyncio
 from . import Device
 from .. import pub
-from ..protocol.serial_protocol import connect
 from ..aldb import ModemALDB
 
 
@@ -18,13 +17,36 @@ class Modem(Device, metaclass=ABCMeta):
         self._protocol = None
         self._transport = None
 
-    @abstractmethod
-    async def connect(self):
+    @property
+    def connected(self) -> bool:
+        """Return true if the transport is connected."""
+        return self._protocol.connected
+
+    def connect(self):
         """Connect to the transport."""
+        asyncio.ensure_future(self.async_connect())
+
+    @abstractmethod
+    async def async_connect(self):
+        """Connect to the transport ascynronously."""
+
+    def close(self):
+        """Close the connection to the transport."""
+        asyncio.ensure_future(self.async_close())
+
+    async def async_close(self):
+        """Close the connection to the transport ascynronously."""
+        pub.unsubscribe(self.connect, 'connection.lost')
+        if self._protocol:
+            self._protocol.close()
+            WAIT_TIME = .0001
+            while self.connected:
+                await asyncio.sleep(WAIT_TIME)
+                WAIT_TIME = min(300, 1.5 * WAIT_TIME)
 
     def _subscribe_topics(self):
         """Subscribe to modem specific topics."""
-        pub.subscribe(self.connect(), "protocol.connection.lost")
+        pub.subscribe(self.connect, "connection.lost")
 
     def _register_states(self):
         """No states to register for modems."""
@@ -44,6 +66,11 @@ class PLM(Modem):
         super().__init__(workdir)
         self._device = device
 
-    async def connect(self):
-        """Connect to the PLM."""
-        self._transport, self._protocol = await connect(self._device)
+    async def async_connect(self):
+        """Connect to the PLM ascynronously."""
+        from ..protocol.serial_protocol import connect as serial_connect
+        self._transport, self._protocol = await serial_connect(self._device)
+        WAIT_TIME = .0001
+        while not self.connected:
+            await asyncio.sleep(WAIT_TIME)
+            WAIT_TIME = min(300, 1.5 * WAIT_TIME)
