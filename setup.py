65f3a017293c7f2ba6798ba77ffab659d608ac2f
Tom Harris <tharris@harrisnj.net>
2019-04-15 20:49:43 -0400

    Set version to alpha 1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name='pyinsteon',
-    version='0.0.2',
+    version='0.0.a1',
     author='The pyinsteon Development Team',
     author_email='pyinsteon@harrisnj.net',
     url='https://github.com/pyinsteon/pyinsteon',
