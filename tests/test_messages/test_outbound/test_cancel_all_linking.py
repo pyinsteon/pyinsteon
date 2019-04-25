54493a3d244dc41bf5f820853d88d0133ad76f3c
Tom Harris <tharris@harrisnj.net>
2019-04-25 16:27:43 -0400

    Fix pylint issues

--- tests/test_messages/test_outbound/test_cancel_all_linking.py
+++ tests/test_messages/test_outbound/test_cancel_all_linking.py
@@ -6,8 +6,9 @@ from tests.test_messages.test_outbound.outbound_base import TestOutboundBase
 
 
 class TestCancelAllLinking(unittest.TestCase, TestOutboundBase):
-
+    """Test Cancel All-Linking command."""
     def setUp(self):
+        #pylint: disable=unused-import
         from pyinsteon.protocol.messages.outbound import cancel_all_linking
         self.hex = '0265'
         super(TestCancelAllLinking, self).base_setup(MessageId(0x65), unhexlify(self.hex))
