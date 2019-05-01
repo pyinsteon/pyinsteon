d24c170ecdc5863de4f4ba4e69be42ad62491a65
Tom Harris <tharris@harrisnj.net>
2019-05-01 11:24:22 -0400

    Add status types 1, 2 and 3

--- pyinsteon/handlers/to_device/status_request.py
+++ pyinsteon/handlers/to_device/status_request.py
@@ -2,7 +2,8 @@
 
 from .. import status_handler
 from .direct_command import DirectCommandHandlerBase
-from ...topics import STATUS_REQUEST
+from ...topics import (STATUS_REQUEST, STATUS_REQUEST_ALTERNATE_1,
+                       STATUS_REQUEST_ALTERNATE_2, STATUS_REQUEST_ALTERNATE_3)
 
 class StatusRequestCommand(DirectCommandHandlerBase):
     """Manage an outbound ON command to a device."""
@@ -10,6 +11,75 @@ class StatusRequestCommand(DirectCommandHandlerBase):
     def __init__(self, address):
         """Init the OnLevelCommand class."""
         super().__init__(address, STATUS_REQUEST)
+
+    #pylint: disable=arguments-differ
+    def send(self):
+        """Send the ON command."""
+        super().send()
+
+    #pylint: disable=arguments-differ
+    async def async_send(self):
+        """Send the ON command async."""
+        return await super().async_send()
+
+    @status_handler
+    def handle_response(self, cmd2, target, user_data):
+        """Handle the ON response direct ACK."""
+        self._call_subscribers(status=cmd2)
+
+
+class StatusRequest1Command(DirectCommandHandlerBase):
+    """Manage an outbound ON command to a device."""
+
+    def __init__(self, address):
+        """Init the OnLevelCommand class."""
+        super().__init__(address, STATUS_REQUEST_ALTERNATE_1)
+
+    #pylint: disable=arguments-differ
+    def send(self):
+        """Send the ON command."""
+        super().send()
+
+    #pylint: disable=arguments-differ
+    async def async_send(self):
+        """Send the ON command async."""
+        return await super().async_send()
+
+    @status_handler
+    def handle_response(self, cmd2, target, user_data):
+        """Handle the ON response direct ACK."""
+        self._call_subscribers(status=cmd2)
+
+
+class StatusRequest2Command(DirectCommandHandlerBase):
+    """Manage an outbound ON command to a device."""
+
+    def __init__(self, address):
+        """Init the OnLevelCommand class."""
+        super().__init__(address, STATUS_REQUEST_ALTERNATE_2)
+
+    #pylint: disable=arguments-differ
+    def send(self):
+        """Send the ON command."""
+        super().send()
+
+    #pylint: disable=arguments-differ
+    async def async_send(self):
+        """Send the ON command async."""
+        return await super().async_send()
+
+    @status_handler
+    def handle_response(self, cmd2, target, user_data):
+        """Handle the ON response direct ACK."""
+        self._call_subscribers(status=cmd2)
+
+
+class StatusRequest3Command(DirectCommandHandlerBase):
+    """Manage an outbound ON command to a device."""
+
+    def __init__(self, address):
+        """Init the OnLevelCommand class."""
+        super().__init__(address, STATUS_REQUEST_ALTERNATE_3)
 
     #pylint: disable=arguments-differ
     def send(self):
