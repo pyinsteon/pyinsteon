e0562116f345dbbf554aa802a42e25884ff129d3
Tom Harris <tharris@harrisnj.net>
2019-05-10 12:24:11 -0400

    Improve ALDB read management

--- pyinsteon/aldb/__init__.py
+++ pyinsteon/aldb/__init__.py
@@ -73,6 +73,13 @@ class ALDBBase(ABC):
         """Return the expected memory address of the first record."""
         return self._mem_addr
 
+    def high_water_mark_mem_addr(self):
+        """Return the High Water Mark record memory address."""
+        for record in self._records:
+            if record.control_flags.is_high_water_mark:
+                return record.mem_addr
+        return None
+
     @property
     def is_loaded(self) -> bool:
         """Test if the ALDB is loaded."""
@@ -82,25 +89,16 @@ class ALDBBase(ABC):
         """Get the record at address 'mem_addr'."""
         return self._records.get(mem_addr, default)
 
-    def load_saved_records(self, status: ALDBStatus, records: dict):
+    def load_saved_records(self, status: ALDBStatus, records: [ALDBRecord]):
         """Load All-Link records from a dictionary of saved records."""
-        from .control_flags import create_from_byte
         if isinstance(status, ALDBStatus):
             self._status = status
         else:
             self._status = ALDBStatus(status)
         for mem_addr in records:
-            rec = records[mem_addr]
-            control_flags = create_from_byte(int(rec.get('control_flags', 0)))
-            group = int(rec.get('group', 0))
-            rec_addr = rec.get('address', '000000')
-            data1 = int(rec.get('data1', 0))
-            data2 = int(rec.get('data2', 0))
-            data3 = int(rec.get('data3', 0))
-            self[int(mem_addr)] = ALDBRecord(int(mem_addr), control_flags,
-                                             group, rec_addr,
-                                             data1, data2, data3)
-        if self._status == ALDBStatus.LOADED and self._records:
+            record = records[mem_addr]
+            self._records[mem_addr] = record
+        if self.is_loaded and self._records:
             keys = list(self._records.keys())
             keys.sort(reverse=True)
             self._mem_addr = keys[0]
@@ -112,20 +110,18 @@ class ALDB(ALDBBase):
     def __init__(self, address, version=ALDBVersion.v2, mem_addr=0x0fff):
         """Init the ALDB class."""
         from ..managers.read_manager import ALDBReadManager
+        from ..managers.write_manager import ALDBWriteManager
         super().__init__(address=address, version=version, mem_addr=mem_addr)
         self._read_manager = ALDBReadManager(self)
+        self._write_manager = ALDBWriteManager(self)
+        self._dirty_records = {}
 
     def __setitem__(self, mem_addr, record):
         """Add or Update a device in the ALDB."""
         if not isinstance(record, ALDBRecord):
             raise ValueError
 
-        self._records[mem_addr] = record
-
-        # After we add a record test load status
-        if self._status == ALDBStatus.LOADING:
-            return
-        self._set_load_status()
+        self._dirty_records[mem_addr] = record
 
     def load(self, refresh=False, callback: Callable = None):
         """Load the ALDB calling the callback when done."""
@@ -139,11 +135,75 @@ class ALDB(ALDBBase):
         self._status = ALDBStatus.LOADING
         if refresh:
             self._records = {}
-        await self._read_manager.async_read(mem_addr=mem_addr, num_recs=num_recs)
+        async for rec in self._read_manager.async_read(mem_addr=mem_addr, num_recs=num_recs):
+            self._records[rec.mem_addr] = rec
         self._set_load_status()
         if callback:
             callback()
         return self._status
+
+    def write_records(self):
+        """Write modified records to the device."""
+        if self.is_loaded():
+            asyncio.ensure_future(self.async_write_records())
+
+    async def async_write_records(self):
+        """Write modified records to the device."""
+        completed = []
+        for mem_addr in self._dirty_records:
+            record = self._dirty_records[mem_addr]
+            if record.mem_addr == 0x0000:
+                record.mem_addr = self._get_next_mem_addr()
+            # We assume a direct ACK is a confirmation of write.
+            # Should we re-read to ensure it is correct.
+            if await self._write_manager.async_write(record):
+                completed.append(record)
+                self._records[record.mem_addr] = record
+        for record in completed:
+            self._dirty_records.pop(record.mem_addr)
+        return len(completed)
+
+    def add(self, group: int, target: Address, controller: bool = False,
+            data1: int = 0x00, data2: int = 0x00, data3: int = 0x00):
+        """Add an All-Link record.
+
+        This method does not write to the device. To write modifications to the device
+        use `write_records` or `async_write_records`.
+        """
+        from .control_flags import ControlFlags
+        flags = ControlFlags(in_use=True, controller=controller, used_before=True)
+        mem_addr = 0x0000
+
+        rec = ALDBRecord(memory=mem_addr, control_flags=flags, group=group, address=target,
+                         data1=data1, data2=data2, data3=data3)
+        self._dirty_records[rec.mem_addr] = rec
+
+    def remove(self, mem_addr: int):
+        """Remove an All-Link record."""
+        from .control_flags import ControlFlags
+        rec = self._records.get(mem_addr)
+        if not rec:
+            raise IndexError('Memory location not found.')
+        new_flags = ControlFlags(in_use=False, controller=rec.control_flags.is_controller,
+                                 used_before=rec.control_flags.is_used_before)
+        new_rec = ALDBRecord(rec.mem_addr, control_flags=new_flags, group=rec.group,
+                             address=rec.address, data1=rec.data1, data2=rec.data2,
+                             data3=rec.data3)
+        self._dirty_records[new_rec.mem_addr] = new_rec
+
+    def _get_next_mem_addr(self):
+        """Return the next memory slot available."""
+        if not self.is_loaded():
+            return None
+
+        last_record = None
+        for record in self:
+            last_record = record
+            control_flags = record.control_flags
+            if control_flags.is_high_water_mark or not control_flags.in_use:
+                return record.mem_addr
+
+        return last_record.mem_addr - 8
 
     def calc_load_status(self):
         """Test if the ALDB is fully loaded."""
--- pyinsteon/managers/read_manager.py
+++ pyinsteon/managers/read_manager.py
@@ -8,7 +8,7 @@ from ..aldb.aldb_record import ALDBRecord
 RETRIES_ALL_MAX = 5
 RETRIES_ONE_MAX = 20
 RETRIES_WRITE_MAX = 5
-TIMER = 10
+TIMER = 5
 TIMER_INCREMENT = 3
 _LOGGER = logging.getLogger(__name__)
 READ_ALL = 1
@@ -25,6 +25,7 @@ class ALDBReadManager():
         from ..handlers.from_device.receive_aldb_record import ReceiveALDBRecordHandler
         self._aldb = aldb
 
+        self._records = asyncio.Queue()
         self._retries_all = 0
         self._retries_one = 0
         self._retries_write = 0
@@ -34,7 +35,7 @@ class ALDBReadManager():
         self._record_handler = ReceiveALDBRecordHandler(self._aldb.address)
         self._read_handler.subscribe(self._receive_record)
         self._record_handler.subscribe(self._receive_record)
-        self._load_lock = asyncio.Lock()
+        self._timer_lock = asyncio.Lock()
 
     def read(self, mem_addr: int = 0x00, num_recs: int = 0):
         """Read one or more ALDB records.
@@ -64,10 +65,14 @@ class ALDBReadManager():
             self._last_command = READ_ALL
         else:
             self._last_command = READ_ONE
-        await self._load_lock.acquire()
+        self._last_mem_addr = mem_addr
         await self._async_read(mem_addr=mem_addr, num_recs=num_recs)
-        await self._load_lock.acquire()
-        return True
+        while True:
+            record = await self._records.get()
+            if record is None:
+                break
+            else:
+                yield record
 
     async def _async_read(self, mem_addr: int = 0x00, num_recs: int = 0):
         """Perform the device read function."""
@@ -84,13 +89,28 @@ class ALDBReadManager():
     def _receive_record(self, is_response: bool, record: ALDBRecord):
         """Receive an ALDB record."""
         num_recs = len(self._aldb)
-        self._aldb[record.mem_addr] = record
+        self._records.put_nowait(record)
         if num_recs != len(self._aldb):
             _LOGGER.info('Received %d records', len(self._aldb))
+        asyncio.ensure_future(self._release_timer())
+
+    async def _release_timer(self):
+        """Release the lock that tells `_timer` to quit."""
+        if self._last_command == READ_ALL and self._retries_all < RETRIES_ALL_MAX:
+            # Wait to see if more messages come in.
+            await asyncio.sleep(5)
+        if self._timer_lock.locked():
+            self._timer_lock.release()
 
     async def _timer(self, timer, mem_addr, num_recs):
         """Set a timer to confirm if the last get command completed."""
-        await asyncio.sleep(timer)
+        if self._timer_lock.locked():
+            self._timer_lock.release()
+        await self._timer_lock.acquire()
+        try:
+            await asyncio.wait_for(self._timer_lock.acquire(), timer)
+        except asyncio.TimeoutError:
+            pass
         if self._last_command == READ_ALL:
             self._manage_get_all_cmd(mem_addr, num_recs)
         else:
@@ -101,8 +121,9 @@ class ALDBReadManager():
         _LOGGER.debug('In _manage_get_all_cmd')
         if self._aldb.calc_load_status():
             # The ALDB is fully loaded so stop
-            self._load_lock.release()
+            self._records.put_nowait(None)
             return
+
         if self._retries_all < RETRIES_ALL_MAX:
             # Attempt to read all records again
             asyncio.ensure_future(self._async_read(0x0000, 0))
@@ -112,7 +133,8 @@ class ALDBReadManager():
             # Read the next missing record
             next_mem_addr = self._next_missing_record()
             if next_mem_addr is None:
-                _LOGGER.error('Tried to get mem address after HWM')
+                # No records are missing so quit
+                self._records.put_nowait(None)
                 return
             if next_mem_addr == self._last_mem_addr:
                 # We are still trying to get the same record as the last read
@@ -123,7 +145,8 @@ class ALDBReadManager():
                                  next_mem_addr, self._retries_one)
                 else:
                     # Tried to read the same record max times so quit
-                    self._load_lock.release()
+                    self._records.put_nowait(None)
+                    return
             else:
                 # Reading a different record than the last attempt so reset
                 # the retry count
@@ -134,23 +157,25 @@ class ALDBReadManager():
     def _manage_get_one_cmd(self, mem_addr, num_recs):
         """Manage the READ_ONE command process."""
         if self._aldb.get(mem_addr):
-            self._load_lock.release()
-        elif self._retries_one < RETRIES_ONE_MAX:
+            self._records.put_nowait(None)
+            return
+        if self._retries_one < RETRIES_ONE_MAX:
             asyncio.ensure_future(self._async_read(mem_addr=mem_addr, num_recs=num_recs))
             self._retries_one += 1
             _LOGGER.info('Retry reading 0x%04x record %d times',
                          mem_addr, self._retries_one)
         else:
             # Trigger aldb.loaded but this will check the load status.
-            self._load_lock.release()
+            self._records.put_nowait(None)
+            return
 
     def _next_missing_record(self):
         last_addr = 0
         if not self._has_first_record():
             if (self._last_mem_addr == 0x0000 and
-                    self._retries_one >= RETRIES_ONE_MAX):
-                return self._aldb.first_mem_addr
-            return 0x0000
+                    self._retries_one < RETRIES_ONE_MAX):
+                return 0x0000
+            return self._aldb.first_mem_addr
         for mem_addr in self._aldb:
             rec = self._aldb[mem_addr]
             if rec.control_flags.is_high_water_mark:
