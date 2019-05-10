a4f073d9415e0f85854a8760906701e303899735
Tom Harris <tharris@harrisnj.net>
2019-05-10 12:58:43 -0400

    Remove comments

--- pyinsteon/handlers/to_device/enter_linking_mode.py
+++ pyinsteon/handlers/to_device/enter_linking_mode.py
@@ -12,11 +12,8 @@ class EnterLinkingModeCommand(DirectCommandHandlerBase):
         """Init the IdRequest class."""
         super().__init__(address, ENTER_LINKING_MODE)
         self._link_complete_handler = AssignToAllLinkGroupCommand(address)
-        # self._id_response_topic = '{}.{}.broadcast'.format(
-        #     self._address.id, ASSIGN_TO_ALL_LINK_GROUP)
 
     #pylint: disable=arguments-differ
     async def async_send(self, group: int = 0):
         """Send the device ID request asyncronously."""
-        # pub.subscribe(self.receive_id, self._id_response_topic)
         return await super().async_send(group=group)
