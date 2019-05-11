b6d2acdfb5445a04d0b2bf3302ffc9b94eb0a4bd
Tom Harris <tharris@harrisnj.net>
2019-05-11 09:49:13 -0400

    Rename device_mgr to devices

--- samples/load_device_aldb.py
+++ samples/load_device_aldb.py
@@ -15,30 +15,29 @@ HOST = '192.168.1.136'
 USERNAME = 'username'
 PASSWORD = 'password'
 
-DEVICE_ADDRESSES = ['27.C3.87'] # , '45.31.94', '46.2F.24', '13.36.96']
-
 _LOGGER = logging.getLogger(__name__)
 _LOGGER_PYINSTEON = logging.getLogger('pyinsteon')
+_LOGGER_MSG = logging.getLogger('pyinsteon.messages')
 
 
 async def load_database():
     """Load the device databae."""
-    from pyinsteon import device_mgr
+    from pyinsteon import devices
     modem = await async_connect(device=DEVICE)
     # modem = await async_connect(host=HOST,
     #                             username=USERNAME,
     #                             password=PASSWORD)
 
-    await device_mgr.load_devices(workdir=PATH)
-    for address in DEVICE_ADDRESSES:
-        device = device_mgr[address]
-
-        _LOGGER.info('Starting DB load for %s', address)
-        await device.aldb.async_load()
-        _LOGGER.info('ALDB load status: %s', device.aldb.status.name)
+    await devices.async_load(workdir=PATH)
+    for address in devices:
+        device = devices[address]
+        if not device.aldb.is_loaded:
+            _LOGGER.info('\nStarting DB load for %s', address)
+            await device.aldb.async_load()
+            await devices.async_save(workdir=PATH)
+        _LOGGER.info('\nALDB load status for %s: %s', device.address, device.aldb.status.name)
         for mem_addr in device.aldb:
             _LOGGER.info(device.aldb[mem_addr])
-
     await modem.async_close()
 
 def print_topics(topic=pub.AUTO_TOPIC, **kwargs):
@@ -54,8 +53,9 @@ if __name__ == '__main__':
 
     _LOGGER.setLevel(logging.INFO)
     _LOGGER_PYINSTEON.setLevel(logging.INFO)
+    _LOGGER_MSG.setLevel(logging.DEBUG)
 
     # pub.subscribe(print_topics, pub.ALL_TOPICS)
     loop = asyncio.get_event_loop()
-    _LOGGER.info('Running database load method')
+    _LOGGER.info('Loading All-Link database for all devices')
     loop.run_until_complete(load_database())
