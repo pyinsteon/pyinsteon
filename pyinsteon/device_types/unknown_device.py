8085b636aa575353915a04cf5b2da3b22250e709
Tom Harris <tharris@harrisnj.net>
2019-05-24 12:24:42 -0400

    Remove unused methods for registering items

--- pyinsteon/device_types/unknown_device.py
+++ pyinsteon/device_types/unknown_device.py
@@ -6,14 +6,3 @@ from . import Device
 class UnknownDevice(Device):
     """Dummy device for testing."""
 
-    def _register_default_links(self):
-        """Do nothing."""
-
-    def _register_handlers(self):
-        """Do nothing."""
-
-    def _register_states(self):
-        """Do nothing."""
-
-    def _register_events(self):
-        """Do nothing."""
