c1442e44fabbd719c15a147ba3e6e9dbee9fd798
Tom Harris <tharris@harrisnj.net>
2019-05-24 12:35:14 -0400

    add topic logger and hub secrets

--- samples/__init__.py
+++ samples/__init__.py
@@ -3,22 +3,33 @@ import logging
 import os
 import sys
 
+from pyinsteon import pub
+
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'pyinsteon'))
 _LOGGER = logging.getLogger(__name__)
 _LOGGER_PYINSTEON = logging.getLogger('pyinsteon')
 _LOGGER_MESSAGES = logging.getLogger('pyinsteon.messages')
+_LOGGER_TOPICS = logging.getLogger('topics')
 PATH = os.getcwd()
 
 
-def set_log_levels(logger='info', logger_pyinsteon='info', logger_messages='info'):
+def set_log_levels(logger='info', logger_pyinsteon='info', logger_messages='info', logger_topics=None):
     """Set the log levels of the three logs."""
     _setup_logger(_LOGGER, logger)
     _setup_logger(_LOGGER_PYINSTEON, logger_pyinsteon)
     _setup_logger(_LOGGER_MESSAGES, logger_messages)
+    if logger_topics:
+        _setup_logger(_LOGGER_TOPICS, 'info')
+        pub.subscribe(_log_all_topics, pub.ALL_TOPICS)
+    else:
+        _setup_logger(_LOGGER_TOPICS, 'fatal')
 
+def _log_all_topics(topic=pub.AUTO_TOPIC, **kwargs):
+    """Log all topics from pyinsteon."""
+    _LOGGER.info('Topic: %s data: %s', topic.name, kwargs)
 
 def _setup_logger(logger, level):
-    _LOGGER.setLevel(_text_to_log_level(level))
+    logger.setLevel(_text_to_log_level(level))
     if not logger.hasHandlers():
         stream_handler = logging.StreamHandler(sys.stdout)
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -42,3 +53,14 @@ def _text_to_log_level(log_level_text):
     if log_level_text.lower() == 'fatal':
         return logging.FATAL
     return logging.INFO
+
+def get_hub_config():
+    """Read the secrets file and return the username, password and address."""
+    import yaml
+    with open("secret.yaml", 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            _LOGGER.error(exc)
+        return (config.get('username'), config.get('password'), config.get('address'))
+        
