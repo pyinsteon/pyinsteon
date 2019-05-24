1fb2d7031b44c39526b1f6033473ed0e051bb246
Tom Harris <tharris@harrisnj.net>
2019-05-24 12:29:16 -0400

    Expose protocol and transport

--- pyinsteon/device_types/modem_base.py
+++ pyinsteon/device_types/modem_base.py
@@ -10,20 +10,45 @@ class ModemBase(Device, metaclass=ABCMeta):
 
     __meta__ = ABCMeta
 
-    def __init__(self, protocol, transport, address='000000',
-                 cat=0x03, subcat=0x00, firmware=0x00,
+    def __init__(self, address='000000', cat=0x03, subcat=0x00, firmware=0x00,
                  description='', model=''):
         """Init the Modem class."""
         super().__init__(address, cat, subcat, firmware, description, model)
         self._aldb = ModemALDB(self._address)
         self._subscribe_topics()
-        self._protocol = protocol
-        self._transport = transport
+        self._protocol = None
+        self._transport = None
 
     @property
     def connected(self) -> bool:
         """Return true if the transport is connected."""
+        if not self._protocol:
+            return False
         return self._protocol.connected
+
+    @property
+    def protocol(self):
+        """Return the protocol."""
+        return self._protocol
+
+    @protocol.setter
+    def protocol(self, value):
+        """Set the protocol."""
+        from ..protocol.protocol import Protocol
+        if isinstance(value, Protocol):
+            self._protocol = value
+
+    @property
+    def transport(self):
+        """Return the transport."""
+        return self._transport
+
+    @transport.setter
+    def transport(self, value):
+        """Set the transport."""
+        from asyncio import Transport
+        if isinstance(value, Transport):
+            self._transport = value
 
     def close(self):
         """Close the connection to the transport."""
@@ -38,6 +63,12 @@ class ModemBase(Device, metaclass=ABCMeta):
             while self.connected:
                 await asyncio.sleep(WAIT_TIME)
                 WAIT_TIME = min(300, 1.5 * WAIT_TIME)
+
+    async def async_get_operating_flags(self):
+        """Read the device operating flags."""
+
+    async def async_set_operating_flags(self):
+        """Write the operating flags to the device."""
 
     def _subscribe_topics(self):
         """Subscribe to modem specific topics."""
@@ -54,3 +85,6 @@ class ModemBase(Device, metaclass=ABCMeta):
 
     def _register_events(self):
         """Register events for modems."""
+
+    def _register_operating_flags(self):
+        """Register operating flags for modem."""
