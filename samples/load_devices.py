c6064a8527af161089782d2c0ea31325fc8d31a0
Tom Harris <tharris@harrisnj.net>
2019-05-27 18:04:46 -0400

    pylint fix

--- samples/load_devices.py
+++ samples/load_devices.py
@@ -28,6 +28,7 @@ async def do_run():
 
 
 if __name__ == '__main__':
-    set_log_levels(logger='info', logger_pyinsteon='info', logger_messages='info', logger_topics=False)
+    set_log_levels(logger='info', logger_pyinsteon='info',
+                   logger_messages='info', logger_topics=False)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(do_run())
