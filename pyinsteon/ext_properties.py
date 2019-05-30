9b0504f9facd50b9176b3fcf604efc2e7567e3b6
Tom Harris <tharris@harrisnj.net>
2019-05-30 13:49:21 -0400

    Add additional property names

--- pyinsteon/ext_properties.py
+++ pyinsteon/ext_properties.py
@@ -4,6 +4,15 @@ X10_HOUSE = 'x10_house'
 X10_UNIT = 'x10_unit'
 LED_DIMMING = 'led_dimming'
 ON_LEVEL = 'on_level'
+AWAKE_INTERVAL = 'awake_interval'
+SLEEP_INTERVAL = 'sleep_interval'
+BROADCAST_NUMBER = 'broadcast_number'
+TRIGGER_GROUP_BIT_MASK = 'trigger_group_bit_mask'
+LSB_OF_SLEEP_INTERVAL = 'lsb_of_sleep_interval'
+APP_RETRIES = 'app_retries'
+CONFIG = 'config'
+BATTERY_LEVEL = 'config_battery_level'
+
 
 class ExtendedProperties():
     """Device extended properties."""
