d3c5d2f5881d2a5486a5e674ef6b45785112886a
Tom Harris <tharris@harrisnj.net>
2019-05-30 13:52:24 -0400

    Handle awake device at device level

--- pyinsteon/aldb/aldb_battery.py
+++ pyinsteon/aldb/aldb_battery.py
@@ -6,10 +6,6 @@ from .. import pub
 from . import ALDBVersion
 from . import ALDB
 
-LOAD_ACTION = 'load'
-WRITE_ACTION = 'write'
-CLOSE_ACTION = 'close'
-
 LoadCommandParams = namedtuple('LoadCommandParams', 'mem_addr num_recs refresh callback')
 Command = namedtuple('Command', 'action params')
 
@@ -20,43 +16,17 @@ class ALDBBattery(ALDB):
         """Init the ALDBBattery class."""
         super().__init__(address=address, version=version, mem_addr=mem_addr)
         self._commands = []
-        self._command_queue = asyncio.Queue()
-        asyncio.ensure_future(self._wait_command())
+        self._load_topic = '{}.aldb.load'.format(self._address.id)
+        self._write_topic = '{}.aldb.write'.format(self._address.id)
 
     async def async_load(self, mem_addr: int = 0x00, num_recs: int = 0x00,
                          refresh: bool = False, callback: Callable = None):
         """Load the All-Link Database."""
-        params = LoadCommandParams(mem_addr=mem_addr, num_recs=num_recs,
-                                   refresh=refresh, callback=callback)
-        cmd = Command(LOAD_ACTION, params)
-        self._commands.append(cmd)
+        pub.sendMessage(self._load_topic, mem_addr=mem_addr, num_recs=num_recs,
+                        refresh=refresh, callback=callback)
         return True
 
     async def async_write_records(self):
         """Write modified records to the device."""
-        cmd = Command(WRITE_ACTION, None)
-        self._commands.append(cmd)
+        pub.sendMessage(self._write_topic)
         return True
-
-    def close(self):
-        """Close the wait command method."""
-        cmd = Command(CLOSE_ACTION, None)
-        self._command_queue.put_nowait(cmd)
-
-    async def _wait_command(self):
-        """Wait for a command."""
-        while True:
-            cmd = await self._command_queue.get()
-            if cmd.action == LOAD_ACTION:
-                p = cmd.params
-                await super().async_load(mem_addr=p.mem_addr, num_recs=p.num_recs,
-                                         refresh=p.refresh, callback=p.callback)
-            elif cmd.action == WRITE_ACTION:
-                await super().async_write_records()
-            else:
-                break
-
-    def _wake_on_device(self, topic=pub.AUTO_TOPIC, **kwargs):
-        """A device is awake and an action needs to be performed."""
-        for cmd in self._commands:
-            self._command_queue.put_nowait(cmd)
