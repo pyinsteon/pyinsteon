df833def4ee3c34e0a5e99b6852de6ee74e11895
Tom Harris <tharris@harrisnj.net>
2019-12-07 23:33:29 -0500

    Split trigger scene on/off

--- pyinsteon/handlers/to_device/trigger_scene_on.py
+++ pyinsteon/handlers/to_device/trigger_scene_on.py
@@ -1,46 +1,46 @@
-"""KeypadLinc command handler to set the LED on/off values."""
+"""KeypadLinc command handler to trigger a button scene."""
 from ..to_device.direct_command import DirectCommandHandlerBase
-from .. import direct_ack_handler
-from ...topics import EXTENDED_GET_SET
+from .. import direct_ack_handler, ack_handler
+from ...topics import EXTENDED_TRIGGER_ALL_LINK
 from ...utils import build_topic
 from ...constants import MessageFlagType
 
 
-class TriggerSceneCommandHandler(DirectCommandHandlerBase):
-    """Set the LED values of a KeypadLinc device.
-
-    TODO make compatable with the single handler per group model
-    """
+class TriggerSceneOnCommandHandler(DirectCommandHandlerBase):
+    """KeypadLinc command handler to trigger a button scene."""
 
     def __init__(self, address, group):
         """Init the SetLedCommandHandler class."""
-        super().__init__(topic=EXTENDED_GET_SET, address=address)
-        self._group = group
+        super().__init__(topic=EXTENDED_TRIGGER_ALL_LINK, address=address, group=group)
         self._on_level = 0
-        self._subscriber_topic = build_topic(
-            prefix="subscriber.{}".format(self._address), # Force address
-            topic="trigger_scene_command",
-            message_type=MessageFlagType.DIRECT,
-        )
 
     # pylint: disable=arguments-differ
-    async def async_send(self, on_level: int = 0xFF, fast_on: bool = False):
+    async def async_send(self, on_level: int = 0xFF, fast: bool = False):
         """Set the LED values of the KPL."""
         self._on_level = on_level
         action = 0 if on_level == 0xFF else 1
-        cmd1 = 0x11 if on_level else 0x13
         cmd2 = self._on_level
-        ramp_rate = 1 if fast_on else 0
+        ramp_rate = 1 if fast else 0
         kwargs = {
             "data1": self._group,
             "data2": action,
             "data3": self._on_level,
-            "data4": cmd1,
+            "data4": 0x11,
             "data5": cmd2,
             "data6": ramp_rate,
         }
 
         return await super().async_send(**kwargs)
+
+    @ack_handler
+    def handle_ack(self, cmd1, cmd2, user_data):
+        """Handle the ACK response.
+
+        Required since the BEEP command uses the same cmd1, cmd2 values.
+        """
+        if not user_data and not user_data['data1'] == self._group:
+            return
+        return super().handle_ack(cmd1, cmd2, user_data)
 
     @direct_ack_handler
     def handle_direct_ack(self, cmd1, cmd2, target, user_data):
