3ebf131616de984bace6b2ac33a3ee3197e8f60f
Tom Harris <tharris@harrisnj.net>
2019-12-14 11:17:32 -0500

    Pull out of link_manager

---
+++ pyinsteon/default_link.py
@@ -0,0 +1,9 @@
+"""Container for a default link record."""
+from collections import namedtuple
+
+
+DefaultLink = namedtuple(
+    "DefaultLink",
+    "is_controller group dev_data1 dev_data2 dev_data3 "
+    "modem_data1 modem_data2 modem_data3",
+)
