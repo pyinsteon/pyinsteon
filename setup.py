1dbaee6065e456dcd6ca3f0e9301d3b547e6022e
Tom Harris <tharris@harrisnj.net>
2019-12-24 14:01:27 -0500

    Black

--- setup.py
+++ setup.py
@@ -5,37 +5,36 @@ from setuptools import setup, find_packages
 
 def readme():
     """Return README file as a string."""
-    with open('README.rst', 'r') as readme_file:
+    with open("README.rst", "r") as readme_file:
         return readme_file.read()
 
 
 setup(
-    name='pyinsteon',
-    version='0.1.0rc2',
-    author='The pyinsteon Development Team',
-    author_email='pyinsteon@harrisnj.net',
-    url='https://github.com/pyinsteon/pyinsteon',
+    name="pyinsteon",
+    version="0.1.0rc2",
+    author="The pyinsteon Development Team",
+    author_email="pyinsteon@harrisnj.net",
+    url="https://github.com/pyinsteon/pyinsteon",
     license="MIT License",
-    packages=find_packages(exclude=['tests', 'tests.*', 'topics',
-                                    'docs', 'samples']),
+    packages=find_packages(exclude=["tests", "tests.*", "topics", "docs", "samples"]),
     scripts=[],
-    description='Python API for controlling Insteon devices',
+    description="Python API for controlling Insteon devices",
     long_description=readme(),
     include_package_data=True,
     zip_safe=True,
-
     install_requires=[
-        'pyserial',
-        'pyserial-asyncio',
-        'aiohttp',
-        'pypubsub',
-        'aiofile',
-        'async_generator',
-        'pyyaml'
-        ],
+        "pyserial",
+        "pyserial-asyncio",
+        "aiohttp",
+        "pypubsub",
+        "aiofile",
+        "async_generator",
+        "pyyaml",
+    ],
     entry_points={
-        'console_scripts': ['insteon_monitor = pyinsteon.tools:monitor',
-                            'insteon_tools = '
-                            'pyinsteon.tools:tools']
-    }
+        "console_scripts": [
+            "insteon_monitor = pyinsteon.tools:monitor",
+            "insteon_tools = " "pyinsteon.tools:tools",
+        ]
+    },
 )
