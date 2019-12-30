c69d4b64c84f5e7c8dda157a3a8da09571a68e45
Tom Harris <tharris@harrisnj.net>
2019-12-29 22:19:37 -0500

    add subscribe on off to X10AllLightsOnOffManager

--- pyinsteon/managers/x10_manager.py
+++ pyinsteon/managers/x10_manager.py
@@ -95,6 +95,16 @@ class X10AllLightsOnOffManager(SubscriberBase):
         pub.subscribe(self._on_received, on_topic)
         pub.subscribe(self._off_received, off_topic)
 
+    def subscribe_on(self, callback):
+        """Subscribe to ON events."""
+        topic = "{}.{}".format(self._subscriber_topic, str(X10Commands.ALL_LIGHTS_ON))
+        pub.subscribe(callback, topic)
+
+    def subscribe_off(self, callback):
+        """Subscribe to OFF events."""
+        topic = "{}.{}".format(self._subscriber_topic, str(X10Commands.ALL_LIGHTS_OFF))
+        pub.subscribe(callback, topic)
+
     def _on_received(self):
         """Receive an ON message."""
         self._call_subscribers(on_level=0xFF)
