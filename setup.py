fc1dc0a9bf1ece163b0f4658752bad430ce43837
Tom Harris <tharris@harrisnj.net>
2020-02-01 22:16:03 -0500

    Bump version to rc4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name="pyinsteon",
-    version="0.1.0rc3",
+    version="0.1.0rc4",
     author="The pyinsteon Development Team",
     author_email="pyinsteon@harrisnj.net",
     url="https://github.com/pyinsteon/pyinsteon",
