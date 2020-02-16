fea52ca51f34fb277318db0663238c362c11c65d
Tom Harris <tharris@harrisnj.net>
2020-02-16 00:54:31 -0500

    Add tools command line

--- pyinsteon/tools.py
+++
@@ -1,9 +0,0 @@
-"""Tools to monitor Insteon messages or interact with library."""
-
-
-def monitor():
-    """Monitor inbound Insteon messages."""
-
-
-def interactive():
-    """Interactive insteon command line."""
