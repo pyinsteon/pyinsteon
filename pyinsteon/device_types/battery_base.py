3efe39ce81cd94be8ca1ff4baccd1764a89ba4b6
Tom Harris <tharris@harrisnj.net>
2020-03-26 09:41:32 -0400

    Clean up command execution logic

--- pyinsteon/device_types/battery_base.py
+++ pyinsteon/device_types/battery_base.py
@@ -5,7 +5,6 @@ import logging
 from functools import partial
 from inspect import iscoroutine, iscoroutinefunction
 
-from .. import pub
 from ..utils import subscribe_topic
 from ..aldb.aldb_battery import ALDBBattery
 from ..constants import ResponseStatus
@@ -13,6 +12,20 @@ from ..handlers.to_device.extended_set import ExtendedSetCommand
 
 _LOGGER = logging.getLogger(__name__)
 TIMEOUT = 2
+
+
+async def _run_command(command):
+    """Run an individual command."""
+    if isinstance(command, partial):
+        _LOGGER.error("Command is a partial")
+        if iscoroutine(command.func) or iscoroutinefunction(command.func):
+            _LOGGER.error("Command is a coroutine (partial)")
+            return await command()
+    if iscoroutine(command) or iscoroutinefunction(command):
+        _LOGGER.error("Command is a coroutine (not partial)")
+        return await command()
+    _LOGGER.error("Command is just a command")
+    return command()
 
 
 # pylint: disable=no-member
@@ -40,6 +53,9 @@ class BatteryDeviceBase:
         self._last_run = None
         self._keep_awake_cmd = ExtendedSetCommand(self._address, data1=0, data2=0x04)
         subscribe_topic(self._device_awake, self._address.id)
+        # This may or may not make sense.
+        # It it helps to set default links when the device first connect,
+        # then this probably makes sense.
         asyncio.ensure_future(self.async_keep_awake())
 
     def _run_on_wake(self, command, retries=3, **kwargs):
@@ -64,41 +80,43 @@ class BatteryDeviceBase:
 
     async def async_keep_awake(self, awake_time=0xFF):
         """Keep the device awake to ensure commands are heard."""
-        return await self._keep_awake_cmd.async_send(data3=awake_time)
+        return await self._keep_awake_cmd.async_send(data3=awake_time, priority=1)
 
-    def _device_awake(self, topic=pub.AUTO_TOPIC, **kwargs):
+    def _device_awake(self, **kwargs):
         """Execute the commands that were requested while sleeping."""
+        if self._commands_queue.empty():
+            return
         if self._last_run is None or self._last_run.done():
-            self._last_run = asyncio.ensure_future(self._run_commands())
+            _LOGGER.error("We have commands to run so let's get to it")
+            self._last_run = asyncio.ensure_future(self._ensure_commands())
+
+    async def _ensure_commands(self):
+        """Ensure any commands are run."""
+        keep_awake_retry = 3
+        while keep_awake_retry:
+            result = await self.async_keep_awake()
+            _LOGGER.error("Keep awake result: %s", str(result))
+            if result == ResponseStatus.SUCCESS:
+                return await self._run_commands()
+            keep_awake_retry -= 1
+            _LOGGER.error("Retries: %d", keep_awake_retry)
+            await asyncio.sleep(3)
 
     async def _run_commands(self):
+        """Run the commands in queue."""
         retry_cmds = []
-        try:
-            while True:
-                keep_awake_retry = 0
-                while keep_awake_retry < 3:
-                    await asyncio.sleep(2)
-                    response = await self.async_keep_awake()
-                    if response == ResponseStatus.SUCCESS:
-                        break
-                    keep_awake_retry += 1
-                if keep_awake_retry == 3:
-                    return
+        while True:
+            try:
                 command, retries = await asyncio.wait_for(
                     self._commands_queue.get(), TIMEOUT
                 )
-                if isinstance(command, partial):
-                    if iscoroutine(command.func) or iscoroutinefunction(command.func):
-                        result = await command()
-                    else:
-                        result = command()
-                elif iscoroutine(command) or iscoroutinefunction(command):
-                    result = await command()
-                else:
-                    result = command()
-                if int(result) != 1 and retries:
+                _LOGGER.error("got a command to run YAY")
+                _LOGGER.error(str(command))
+                result = await _run_command(command)
+                if result != ResponseStatus.SUCCESS and retries:
                     retry_cmds.append((command, retries - 1))
-        except asyncio.TimeoutError:
-            pass
+            except asyncio.TimeoutError:
+                break
+
         for cmd, retries in retry_cmds:
             self._run_on_wake(cmd, retries)
