617e8281ba10528425d12db80b5d41e78aaff580
Tom Harris <tharris@harrisnj.net>
2020-03-31 13:49:07 -0400

    Raise ValueError rather than general exception

--- pyinsteon/handlers/to_device/extended_set.py
+++ pyinsteon/handlers/to_device/extended_set.py
@@ -16,7 +16,7 @@ class ExtendedSetCommand(DirectCommandHandlerBase):
         """Init the ReadALDBCommandHandler."""
         if data2 in [0, 1]:
             _LOGGER.error("Extended Set sent with bad action number: %d", data1)
-            raise (Exception("Error creating extended set command"))
+            raise (ValueError("Error creating extended set command"))
         super().__init__(topic=EXTENDED_GET_SET, address=address, group=0)
         self._data1 = data1
         self._data2 = data2
@@ -45,7 +45,7 @@ class ExtendedSetCommand(DirectCommandHandlerBase):
             try:
                 data = int(loc["data{}".format(item)])
             except ValueError:
-                _LOGGER.error("Property value must be an integer")
+                _LOGGER.error("Property value must be an integer: %s", str(item))
             else:
                 kwargs["data{}".format(item)] = data
         return await super().async_send(priority=5, **kwargs)
