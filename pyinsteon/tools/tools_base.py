6f7f50308586659dd61ac7be78c9cfb47e37da22
Tom Harris <tharris@harrisnj.net>
2020-04-15 23:23:56 -0400

    Reuse stdin and stdout across menus

--- pyinsteon/tools/tools_base.py
+++ pyinsteon/tools/tools_base.py
@@ -24,9 +24,11 @@ CmdArgs = namedtuple("CmdArgs", "workdir device username host hub_version port")
 class ToolsBase(Cmd):
     """Base class for all tools menues."""
 
-    def __init__(self, loop, args=None, menu=None):
+    def __init__(self, loop, args=None, menu=None, stdin=None, stdout=None):
         """Init the InsteonCmd class."""
         super().__init__()
+        self.stdin = stdin
+        self.stdout = stdout
         prompt = "pyinsteon"
         if menu:
             self.prompt = f"{prompt} - {menu}: "
@@ -67,9 +69,10 @@ class ToolsBase(Cmd):
         """
 
         self.preloop()
-        self.stdin, self.stdout = await stdio(loop=self.loop)
-        if sys.platform != "win32":
-            patch_stdin_stdout(self.stdin, self.stdout)
+        if not self.stdin and not self.stdout:
+            self.stdin, self.stdout = await stdio(loop=self.loop)
+            if sys.platform != "win32":
+                patch_stdin_stdout(self.stdin, self.stdout)
 
         try:
             if intro is not None:
@@ -461,7 +464,9 @@ class ToolsBase(Cmd):
             self.hub_version,
             self.port,
         )
-        await menu(self.loop, cmd_args, name).start_menu()
+        await menu(
+            self.loop, cmd_args, name, stdin=self.stdin, stdout=self.stdout
+        ).start_menu()
 
     def _add_filter(self):
         """Add a filter for the current menu."""
