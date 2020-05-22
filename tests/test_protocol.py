a840adabaa9626cbd622a687358685815e961dd4
Tom Harris <tharris@harrisnj.net>
2020-05-21 22:45:46 -0400

    Make more consistant

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -34,25 +34,25 @@ class TestProtocol(unittest.TestCase):
         )
         self._protocol = Protocol(connect_method=self._connect_method)
         self._last_topic = ""
-        set_log_levels(logger_topics=False)
+        set_log_levels(logger_topics=True)
 
     @async_case
     async def test_send_on_topic(self):
         """Test sending the ON command."""
         received_topic = ""
 
-        def expected_topic_received(cmd1, cmd2, user_data, topic=pub.AUTO_TOPIC):
+        def expected_topic_received(msg, priority=5, topic=pub.AUTO_TOPIC):
             nonlocal received_topic
             received_topic = topic.name
 
         address = random_address()
-        on_topic = "send.{}.1".format(ON)
+        on_topic = "send.{}.1.direct".format(ON)
         topics = [
             TopicItem(on_topic, {"address": address, "on_level": 0xFF, "group": 0}, 0)
         ]
         await self._protocol.async_connect()
         self._last_topic = None
-        expected_topic = "ack.{}.1.on.direct".format(address.id)
+        expected_topic = "send_message.on.1"
         pub.subscribe(expected_topic_received, expected_topic)
         send_topics(topics)
         await asyncio.sleep(0.05)
@@ -65,7 +65,9 @@ class TestProtocol(unittest.TestCase):
         """Test receiving an ON message."""
         last_topic = None
 
-        def topic_received(topic=pub.AUTO_TOPIC, **kwargs):
+        def topic_received(
+            cmd1, cmd2, user_data, target, hops_left, topic=pub.AUTO_TOPIC
+        ):
             """Receive the OFF topic for a device."""
             nonlocal last_topic
             last_topic = topic.name
@@ -74,13 +76,14 @@ class TestProtocol(unittest.TestCase):
         byte_data = create_std_ext_msg(
             address, 0x80, 0x11, 0xFF, target=Address("000001")
         )
-        pub.subscribe(topic_received, address.id)
+        expected_topic = "{}.{}.on.broadcast".format(address.id, 1)
+        pub.subscribe(topic_received, expected_topic)
         on_cmd = DataItem(byte_data, 0)
         data = [on_cmd]
         await self._protocol.async_connect()
         send_data(data, self._read_queue)
         await asyncio.sleep(0.05)
-        assert last_topic == "{}.{}.on.broadcast".format(address.id, 1)
+        assert last_topic == expected_topic
         self._protocol.close()
         await asyncio.sleep(0.1)
 
@@ -93,7 +96,7 @@ class TestProtocol(unittest.TestCase):
 
         last_topic = None
 
-        def topic_received(topic=pub.AUTO_TOPIC, **kwargs):
+        def topic_received(cmd1, cmd2, user_data, topic=pub.AUTO_TOPIC):
             """Receive the OFF topic for a device."""
             nonlocal last_topic
             last_topic = topic.name
@@ -101,7 +104,7 @@ class TestProtocol(unittest.TestCase):
         group = 3
         target = Address(bytearray([0x00, 0x00, group]))
         ack_topic = "ack.{}.on.all_link_broadcast".format(target.id)
-        pub.subscribe(topic_received, f"ack.{target.id}")
+        pub.subscribe(topic_received, ack_topic)
         cmd = OnLevelAllLinkBroadcastCommand(group=group)
         await self._protocol.async_connect()
         await cmd.async_send()  # Mock transport auto sends ACK/NAK
