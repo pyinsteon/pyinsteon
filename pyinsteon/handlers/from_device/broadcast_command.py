5de8a33c7eb0858f5ad647c2ada62460ae83da75
Tom Harris <tharris@harrisnj.net>
2020-06-21 22:10:05 -0400

    Move dedup test to base broadcast base class

--- pyinsteon/handlers/from_device/broadcast_command.py
+++ pyinsteon/handlers/from_device/broadcast_command.py
@@ -1,4 +1,5 @@
 """Base class to handle Broadcast messages from devices."""
+from datetime import datetime
 from ...address import Address
 from ...constants import MessageFlagType
 from ..inbound_base import InboundHandlerBase
@@ -17,3 +18,27 @@ class BroadcastCommandHandlerBase(InboundHandlerBase):
             group=self._group,
             message_type=MessageFlagType.ALL_LINK_BROADCAST,
         )
+        self._last_command = datetime(1, 1, 1)
+        self._last_hops_left = None
+
+    def is_first_message(self, **kwargs):
+        """Test if the message is a duplicate."""
+        curr_time = datetime.now()
+        tdelta = curr_time - self._last_command
+        self._last_command = curr_time
+        hops_left = kwargs["hops_left"]
+        if (
+            kwargs["target"].middle != 0
+            and hops_left == self._last_hops_left
+            and tdelta.seconds < 1
+        ):
+            return False
+        if (
+            self._last_hops_left is None
+            or (hops_left == self._last_hops_left and tdelta.seconds > 0.7)
+            or hops_left > self._last_hops_left
+            or tdelta.seconds >= 2
+        ):
+            self._last_hops_left = hops_left
+            return True
+        return False
