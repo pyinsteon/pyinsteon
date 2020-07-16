28b6ad6fc162d5220fcc6b4c711331c27303cbef
Tom Harris <tharris@harrisnj.net>
2020-07-16 17:31:06 -0400

    Bump version to 1.0.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name="pyinsteon",
-    version="1.0.5",
+    version="1.0.6",
     author="The pyinsteon Development Team",
     author_email="pyinsteon@harrisnj.net",
     url="https://github.com/pyinsteon/pyinsteon",
