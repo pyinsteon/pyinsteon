5634b210c7c206eff7113957a2ecca39041a04a3
Tom Harris <tomharris@harrisnj.net>
2023-02-09 15:05:37 +0000

    Only poke if mode is PEEK/POKE

--- pyinsteon/managers/aldb_write_manager.py
+++ pyinsteon/managers/aldb_write_manager.py
@@ -41,20 +41,20 @@ class ALDBWriteManager:
                 "All-Link databased must be loaded before writing."
             )
 
-        if self._aldb.read_write_mode == ReadWriteMode.STANDARD:
-            return await self._write_handler.async_send(
-                mem_addr=record.mem_addr,
-                controller=record.is_controller,
-                group=record.group,
-                target=record.target,
-                data1=record.data1,
-                data2=record.data2,
-                data3=record.data3,
-                in_use=record.is_in_use,
-                bit5=record.is_bit5_set,
-                bit4=record.is_bit4_set,
-            )
-        return await self._poke_record(record)
+        if self._aldb.read_write_mode == ReadWriteMode.PEEK_POKE:
+            return await self._poke_record(record)
+        return await self._write_handler.async_send(
+            mem_addr=record.mem_addr,
+            controller=record.is_controller,
+            group=record.group,
+            target=record.target,
+            data1=record.data1,
+            data2=record.data2,
+            data3=record.data3,
+            in_use=record.is_in_use,
+            bit5=record.is_bit5_set,
+            bit4=record.is_bit4_set,
+        )
 
     async def _poke_record(self, record: ALDBRecord):
         """Write the record using poke commands."""
