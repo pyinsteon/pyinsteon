5a21f73015ab6c8516d7d93fa6c63472adf8273f
Connor Gallopo <connor.gallopo@me.com>
2026-07-30 11:06:59 -0400

    clear low battery on the group 3 off message instead of guessing from traffic

--- pyinsteon/managers/low_batter_manager.py
+++ pyinsteon/managers/low_batter_manager.py
@@ -1,14 +1,9 @@
 """Low battery manager."""
 
-import asyncio
-
 from ..address import Address
 from ..handlers.from_device.off import OffInbound
 from ..handlers.from_device.on_level import OnLevelInbound
 from ..subscriber_base import SubscriberBase
-from ..utils import subscribe_topic
-
-WAIT_TIME = 5
 
 
 class LowBatteryManager(SubscriberBase):
@@ -31,40 +26,27 @@ class LowBatteryManager(SubscriberBase):
         self._on_low_battery = OnLevelInbound(self._address, self._group)
         self._off_low_battery = OffInbound(self._address, self._group)
         self._on_low_battery.subscribe(self._low_battery)
-        self._off_low_battery.subscribe(self._low_battery)
-        self._low_battery_recd = False
+        self._off_low_battery.subscribe(self._low_battery_clear)
         self._low_battery_state = False
         self._low_battery_event = self.LowBatterySubscriber(f"{subscriber_topic}.true")
         self._low_battery_clear_event = self.LowBatterySubscriber(
             f"{subscriber_topic}.false"
         )
-        subscribe_topic(self._all_device_messages, self._address.id)
 
     def subscribe_low_battery_event(self, callback):
         """Subscribe to low battery event."""
         self._low_battery_event.subscribe(callback)
 
     def subscribe_low_battery_clear_event(self, callback):
-        """Subscribe to low battery event."""
+        """Subscribe to low battery clear event."""
         self._low_battery_clear_event.subscribe(callback)
-
-    def _all_device_messages(self, **kwargs):
-        """Capture all messages for this device."""
-        loop = asyncio.get_event_loop()
-        target = kwargs.get("target")
-        # stop if this is a low battery message
-        if target and Address(target).low == self._group:
-            return
-        self._low_battery_recd = False
-        loop.call_later(WAIT_TIME, self._check_low_battery)
 
     def _low_battery(self, on_level):
         """Low battery message received."""
-        self._low_battery_recd = True
         self._low_battery_state = True
         self._low_battery_event.call_subscribers(low_battery=True)
 
-    def _check_low_battery(self):
-        """Check if a low battery message was received since the last message."""
-        if not self._low_battery_recd and self._low_battery_state:
-            self._low_battery_clear_event.call_subscribers(low_battery=False)
+    def _low_battery_clear(self, on_level):
+        """Good battery message received."""
+        self._low_battery_state = False
+        self._low_battery_clear_event.call_subscribers(low_battery=False)
